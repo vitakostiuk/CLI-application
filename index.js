97fbee18fd3000e3ffae2c01e581813ed464fa84
Vita Kostiuk <vitagrebennik@gmail.com>
2022-07-10 14:29:22 +0300

    finish task

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-// const argv = require("yargs").argv;
-const yargs = require("yargs");
-const { hideBin } = require("yargs/helpers");
-
+// Испольован модуль commander для парсинга аргументов командной строки
+//
 const contacts = require("./contacts");
 
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+console.log(argv);
+
+// TODO: рефакторить
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
@@ -31,27 +44,57 @@ async function invokeAction({ action, id, name, email, phone }) {
   }
 }
 
-console.log(process.argv);
-const arr = hideBin(process.argv);
-// console.log(arr);
-const { argv } = yargs(arr);
-// console.log(argv);
 invokeAction(argv);
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "2" });
-// invokeAction({
-//   action: "add",
-//   name: "Vita",
-//   email: "Kostiuk",
-//   phone: "555-88-78",
-// });
-// invokeAction({ action: "remove", id: "1" });
+// Испольован пакет yargs для парсинга аргументов командной строки
+//
+// // const argv = require("yargs").argv;
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
 
-////////////////////////////////////
-// const actionIdx = process.argv.indexOf("--action");
-// if (actionIdx !== -1) {
-//   const action = process.argv[actionIdx + 1];
-//   invokeAction({ action });
+// const contacts = require("./contacts");
+
+// async function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       const contactsList = await contacts.listContacts();
+//       console.log(contactsList);
+//       break;
+
+//     case "get":
+//       const contactById = await contacts.getContactById(id);
+//       console.log(contactById);
+//       break;
+
+//     case "add":
+//       const newContact = await contacts.addContact(name, email, phone);
+//       console.log(newContact);
+//       break;
+
+//     case "remove":
+//       const removeContact = await contacts.removeContact(id);
+//       console.log(removeContact);
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
 // }
-///////////////////////////////////////
+
+// // // Вызов функции invokeAction
+// // invokeAction({ action: "list" });
+// // invokeAction({ action: "get", id: "2" });
+// // invokeAction({
+// //   action: "add",
+// //   name: "Vita",
+// //   email: "Kostiuk",
+// //   phone: "555-88-78",
+// // });
+// // invokeAction({ action: "remove", id: "1" });
+
+// // console.log(process.argv);
+// const arr = hideBin(process.argv);
+// // console.log(arr);
+// const { argv } = yargs(arr);
+// // console.log(argv);
+// invokeAction(argv);
